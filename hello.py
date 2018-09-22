17485e77fcb0b7984cf000551b868f60ba34b0bf
NF404_301 <1984148098@qq.com>
2018-09-22 19:28:21 +0800

    Flask moment|wtf
    Signed-off-by: NF404_301 <1984148098@qq.com>

--- hello.py
+++ hello.py
@@ -1,17 +1,44 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
-@app.route('/')
+
+
+class NameForm(Form):
+    name = StringField('Your name', validators = [Required()])
+    submit = SubmitField('提交')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-
-    return render_template('index.html')
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.date = ''
+    return render_template('index.html',
+                           current_time=datetime.utcnow(),
+                           form = form,
+                           name = name)
 
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html',name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
